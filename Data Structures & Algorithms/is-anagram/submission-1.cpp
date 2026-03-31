0842942e6be4a927ee4d3091a66ae8e38fc816d6
gulshan-tandan62 <blankedcd6.2@gmail.com>
2026-03-31 08:04:21 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        unordered_map<char,int>freq;
+        for(auto ch : s)
+        {
+            freq[ch]++;
+        }
+        for(auto ch : t)
+        {
+           
+                freq[ch]--;
+                if(freq[ch]<0) return false;
+          
+        }
+        return true;
+    }
+};
